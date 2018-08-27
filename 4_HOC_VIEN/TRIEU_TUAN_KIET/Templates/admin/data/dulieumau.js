db77c74a67b4d02a5ff114d7d11592de2f84e53c
trieutuankiet <trieukiet111@gmail.com>
2018-08-27 14:32:02 +0700

    BAI TAP VE NHA

---
+++ 4_HOC_VIEN/TRIEU_TUAN_KIET/Templates/admin/data/dulieumau.js
@@ -0,0 +1,20 @@
+var dlm = function()
+{
+	this.DS_TAIKHOAN = [
+		{
+			TAI_KHOAN:"admin",
+			MAT_KHAU:"123",
+			VAI_TRO:"Quản trị hệ thống",
+			EMAIL:"admin@thegioididong.com",
+			AVATAR:"admin.png"
+		},
+		{
+			TAI_KHOAN:"nhanvien",
+			MAT_KHAU:"123",
+			VAI_TRO:"Nhân viên bán hàng",
+			EMAIL:"nhanvien@thegioididong.com",
+			AVATAR:"nhanvien1.png"
+		}
+	]
+
+}
