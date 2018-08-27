77aeeaf307f30f3f9dfeb6ba6661dbd0e46cc5da
buiduchieuvnu <buiduchieuvnu@gmail.com>
2018-08-27 19:09:44 +0700

    Load danh mục sản phẩm từ dữ liệu mẫu

--- 3_PROJECTS/Templates/admin/data/DULIEU_MAU.js
+++ 3_PROJECTS/Templates/admin/data/DULIEU_MAU.js
@@ -17,4 +17,10 @@ var DULIEU_MAU = function(){
 		}
 	];
 
+	this.DM_SANPHAM = [
+		{MA:'DIENTHOAI',TEN: 'Điện thoại',MO_TA: 'Danh mục điện thoại',TRANG_THAI:'Kích hoạt'},
+		{MA:'LAPTOP',TEN: 'Latop',MO_TA: 'Danh mục máy tính xách tay',TRANG_THAI:'Kích hoạt'},
+		{MA:'PHUKIEN',TEN: 'Phụ kiện',MO_TA: 'Danh mục phụ kiện',TRANG_THAI:'Khóa'},
+		{MA:'TABLET',TEN: 'Máy tính bảng',MO_TA: 'Danh mục máy tính bảng',TRANG_THAI:'Kích hoạt'}
+	];
 }
