9105bb19d73556f4f439e811b9d52b9ab27a1fe3
buiduchieuvnu <buiduchieuvnu@gmail.com>
2018-09-17 19:01:43 +0700

    function.php trong PHP

---
+++ 2_LABS/Buoi_15/function.php
@@ -0,0 +1,36 @@
+<?php
+
+// Giải phương trình bậc 2 - 1 ẩn
+function GiaiPT($a, $b, $c){
+	if($a==0){
+		if($b==0){
+			if($c==0){
+				echo 'Phương trình vô số nghiệm';
+			}else{
+				echo 'Phương trình vô nghiệm';
+			}
+		}else{
+			$n = -$c/$b;
+			echo "Phương có 1 nghiệm: $n";
+		}
+	}else{
+		$delta = $b*$b - 4*$a*$c;
+		if($delta <0 ){
+			echo 'Phương trình vô nghiệm';
+		}else if($delta == 0){
+			$n = -$b/(2*$a);
+			echo "Phương trình có nghiệm kép: $n";
+		}else{
+			$n1 = (-$b - sqrt($delta))/(2*$a);
+			$n2 = (-$b + sqrt($delta))/(2*$a);
+			echo "Phương trình có 2 nghiệm: <br/> N1: $n1 <br/>N2: $n2";
+		}
+	}
+}
+$a = 1;
+$b = 2;
+$c = 1;
+echo "Phương trình: $a * X^2 + $b * X + $c = 0 <br/>";
+GiaiPT($a, $b, $c);
+
+?>
