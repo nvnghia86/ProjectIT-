760ca62d7f4933ee7a15257c517ed0e4dc6b75a4
buiduchieuvnu <buiduchieuvnu@gmail.com>
2018-10-19 20:03:34 +0700

    Sửa link

--- 2_LABS/Buoi_23_PHP_MVC_Ajax/SampleMvcAjax/Views/Default.php
+++ 2_LABS/Buoi_23_PHP_MVC_Ajax/SampleMvcAjax/Views/Default.php
@@ -4,6 +4,6 @@
 		<a href="?controller=khachhang&view=danhsach">Quản lý danh sách khách hàng</a>
 	</li>
 	<li>
-		<a href="?controller=baohanh&view=baohanh_danhsach">Quản lý danh mục bảo hành</a>
+		<a href="?controller=chdmbaohanh&view=danhsach">Quản lý danh mục bảo hành</a>
 	</li>
 </ul>
