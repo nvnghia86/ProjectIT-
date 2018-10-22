c792bb3014d00829bc3e7ff311460d1a7bfddd8b
trinhhang998 <trinhhangbon998@gmail.com>
2018-10-22 20:44:47 +0700

    a

---
+++ 3_PROJECTS/project.itp.com/app/DmSanPham/views/ChiTiet.php
@@ -0,0 +1,30 @@
+<?php if (!defined('AREA')) {
+    die('Access denied');
+} ?>
+<form class="row" style="margin-bottom:5px" id="FORM">
+    <div class="form-group-sm col-sm-6">
+       <label>Tên sản phẩm <span class="require">(*)</span></label>
+	   <input type="text" class="form-control" placeholder="Tên sản phẩm"  />
+    </div>
+    <div class="form-group-sm col-sm-6">
+       <label>id dm sảm phẩm cha <span class="require">(*)</span></label>
+	   <input type="text" class="form-control" placeholder="id_dm_sanpham_cha"  />
+    </div>
+	<div class="form-group-sm col-sm-6">
+       <label>Mô tả <span class="require">(*)</span></label>
+	   <textarea class="form-control" rows="3"></textarea>
+    </div>
+    
+    <div class="form-group-sm col-sm-4">
+        <label>Trạng thái </label>
+        <select class="form-control" id="sel_TRANG_THAI" name="trang_thai">
+            <option value="1">1</option>
+            <option value="0">0</option>
+        </select>
+    </div>
+
+    <div class="form-group-sm ACTIONS col-sm-12">
+        <hr>
+        <button type="button" class="btn btn-warning btn-sm" id="btnSave"><i class="glyphicon glyphicon-save"></i> Lưu</button>
+    </div>
+</form>
