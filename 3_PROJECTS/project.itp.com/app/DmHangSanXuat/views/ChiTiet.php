1830f7e113708a20b3e6af07bcb8cf3012653403
duongviet <duongvanviet.97@gmail.com>
2018-10-22 20:44:33 +0700

    chhangsanxuat

---
+++ 3_PROJECTS/project.itp.com/app/DmHangSanXuat/views/ChiTiet.php
@@ -0,0 +1,27 @@
+<?php if (!defined('AREA')) {
+    die('Access denied');
+} ?>
+
+<form class="row" style="margin-bottom:5px" id="FORM">
+    <div class="form-group-sm col-sm-6">
+       <label>Tên hãng sản xuất <span class="require">(*)</span></label>
+	   <input type="text" class="form-control" placeholder="Tên hãng sản xuất"  />
+    </div>
+	<div class="form-group-sm col-sm-6">
+       <label>Mô tả <span class="require">(*)</span></label>
+	   <textarea class="form-control" rows="3"></textarea>
+    </div>
+    
+    <div class="form-group-sm col-sm-4">
+        <label>Trạng thái </label>
+        <select class="form-control" id="sel_TRANG_THAI" name="trang_thai">
+            <option value="1">Kích hoạt</option>
+            <option value="0">Khóa</option>
+        </select>
+    </div>
+
+    <div class="form-group-sm ACTIONS col-sm-12">
+        <hr>
+        <button type="button" class="btn btn-warning btn-sm" id="btnSave"><i class="glyphicon glyphicon-save"></i> Lưu</button>
+    </div>
+</form>
