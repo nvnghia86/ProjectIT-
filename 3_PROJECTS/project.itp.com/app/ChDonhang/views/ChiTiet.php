71ff8b04ac6472cd7ea4f98cb972151a3b85c7a6
minhlongyb89 <minhlongyb.89@gmail.com>
2018-10-26 20:28:11 +0700

    dsff

--- 3_PROJECTS/project.itp.com/app/ChDonhang/views/ChiTiet.php
+++ 3_PROJECTS/project.itp.com/app/ChDonhang/views/ChiTiet.php
@@ -37,7 +37,10 @@
        <label>Địa chỉ Người Nhận <span class="require">(*)</span></label>
 	   <textarea class="form-control" rows="3"></textarea>
     </div>
-	 <label>Trạng Thái<span class="require">(*)</span></label>
+	<div class="form-group-sm col-sm-6">
+       <label>Trạng Thái<span class="require">(*)</span></label>
+	   <input type="text" class="form-control" placeholder="Tên người nhận"  />
+    </div>
 	<div class="form-group-sm ACTIONS col-sm-12">
         <hr>
         <button type="button" class="btn btn-warning btn-sm" id="btnSave"><i class="glyphicon glyphicon-save"></i> Lưu</button>
