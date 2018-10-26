51167cc86612f6da7d959bea3f573787879a0f5c
duongviet <duongvanviet.97@gmail.com>
2018-10-26 20:26:31 +0700

    viet

--- 3_PROJECTS/project.itp.com/app/DmHangSanXuat/views/DanhSach.php
+++ 3_PROJECTS/project.itp.com/app/DmHangSanXuat/views/DanhSach.php
@@ -17,12 +17,13 @@
         <div class="row panel panel-primary" id="LIST">
             <div class="panel-heading">Danh sách hãng sản xuất</div>
             <div class="panel-body">
-                <table class="table table-bordered table-stripped">
+                <table class="table table-bordered table-stripped" id="DanhSach">
 					<thead>
 						<tr>
 							<th><input type="checkbox" id="chkAll" /></th>
 							<th>STT</th>
 							<th>Tên</th>
+							<th>Ảnh Logo</th>
 							<th>Mô tả</th>
 							<th>Trạng thái</th>
 						</tr>
@@ -32,6 +33,7 @@
 							<td><input type="checkbox" id="chk_1" /></td>
 							<td>1</td>
 							<td>SamSung</td>
+							<td></td>
 							<td>Hãng điện thoại hàng đầu thế giới</td>
 							<td>
 								<span class="label label-success">Sử dụng</span>
@@ -43,9 +45,47 @@
         </div>
     </div>
 </div>
+
+<script src="app/DmHangSanXuat/js/DmHangSanXuat.js"></script>
 <script>
+	// Khai báo đối tượng cửa sổ
 	var EccDialog = new ECC_DIALOG();
+	var HangSanXuat = new DmHangSanXuat('?app=DmHangSanXuat');
+	
+	function Page_init(){
+		HangSanXuat.FindAll();
+		DanhSach_bind();
+	}
+	
+	function DanhSach_bind(){
+		
+		var _html = '';
+		for(var i=0; i< HangSanXuat.DanhSach.length;i++){
+			var _dong = HangSanXuat.DanhSach[i];
+			
+			var _trangthai ='';
+			if(_dong.trangthai=='1'){
+				_trangthai = '<span class="label label-success">Sử dụng</span>';
+			}else{
+				_trangthai = '<span class="label label-danger">Khóa</span>';
+			}
+			
+			_html +='<tr>';
+			_html +='	<td><input type="checkbox" id="chk_1" /></td>';
+			_html +='	<td>'+ (i+1) +'</td>';
+			_html +='	<td>'+ _dong.ten +'</td>';
+			_html +='	<td>'+ _dong.anh_logo +'</td>';
+			_html +='	<td>'+ _dong.mota +'</td>';
+			_html +='	<td>';
+			_html += _trangthai;
+			_html +='	</td>';
+			_html +='</tr>';
+		}
+		$('#DanhSach > tbody').html(_html);
+	}
+
 	$(function(){
+		Page_init();
 		$('#btnThemMoi').on('click',function(){
 			EccDialog.show('Tạo mới danh mục hãng sản xuất','?app=DmHangSanXuat&view=ChiTiet&layout=popup','50%','300');
 		});
