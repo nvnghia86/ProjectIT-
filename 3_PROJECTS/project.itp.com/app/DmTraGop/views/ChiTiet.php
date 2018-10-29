8184f5de531a7703dee446e37f4ef24186724b38
hoanghung1998 <hoangminhhung388@gmail.com>
2018-10-29 20:48:34 +0700

    emm

--- 3_PROJECTS/project.itp.com/app/DmTraGop/views/ChiTiet.php
+++ 3_PROJECTS/project.itp.com/app/DmTraGop/views/ChiTiet.php
@@ -1,22 +1,18 @@
-<?php if (!defined('AREA')) {
-    die('Access denied');
-} ?>
-
-<h1>Chi tiết</h1><?php if (!defined('AREA')) { die('Access denied'); } ?>
+<?php if (!defined('AREA')) { die('Access denied'); } ?>
 
 <form class="row" style="margin-bottom:5px" id="FORM">
     <div class="form-group-sm col-sm-6">
-       <label>Tên Trả Góp<span class="require">(*)</span></label>
-	   <input type="text" class="form-control" placeholder="Tên Trả Góp"  />
+       <label>Tên Trả Gop <span class="require">(*)</span></label>
+	   <input type="text" id="ten" class="form-control" placeholder="Tên Trả Gop"  />
     </div>
 	<div class="form-group-sm col-sm-6">
        <label>Mô tả <span class="require">(*)</span></label>
-	   <textarea class="form-control" rows="3"></textarea>
+	   <textarea id="mota" class="form-control" rows="3"></textarea>
     </div>
     
     <div class="form-group-sm col-sm-4">
         <label>Trạng thái </label>
-        <select class="form-control" id="sel_TRANG_THAI" name="trang_thai">
+        <select class="form-control" id="trangthai">
             <option value="1">Kích hoạt</option>
             <option value="0">Khóa</option>
         </select>
@@ -27,3 +23,46 @@
         <button type="button" class="btn btn-warning btn-sm" id="btnSave"><i class="glyphicon glyphicon-save"></i> Lưu</button>
     </div>
 </form>
+<script src="app/DmTraGop/js/DmTraGop.js"></script>
+<script>
+	
+	var TraGop = new DmTraGop('?app=DmTraGop');
+	
+	function Page_init(){
+		TraGop.id_dm_tragop = Util.getParameterByName('id');
+		if(TraGop.id_dm_tragop==0 || TraGop.id_dm_tragop==null){
+			$('#ten').val('');
+			$('#mota').val('');
+			$('#trangthai').val('1');
+		}else{
+			TraGop.GetById();
+			$('#ten').val(TraGop.ten);
+			$('#mota').val(TraGop.mota);
+			$('#trangthai').val(TraGop.trangthai);
+		}
+	}
+	
+	$(function(){
+		
+		Page_init();
+		
+		$('#btnSave').on('click',function(){
+			TraGop.ten = $('#ten').val();
+			TraGop.mota = $('#mota').val();
+			TraGop.trangthai = $('#trangthai').val();
+			TraGop.Save();
+		})
+		
+	})
+
+</script>
+
+
+
+
+
+
+
+
+
+
