f6e3c62a33abc0d2b0e521ea45a6f45b488ee789
buiduchieuvnu <buiduchieuvnu@gmail.com>
2018-11-26 18:52:13 +0700

    Chi tiết sản phâm

---
+++ 3_PROJECTS/project.itp.com/app/GianHang/GianHang.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * App quản lý danh mục bảo hành
+ * Create: 08-10-2017
+ * @author Hieubd <buiduchieuvnu@gmail.com>
+ * @version 1.0
+ */
+session_start();
+ob_start();
+
+if ( !defined('AREA') ) {
+    die('Access denied');
+}
+
+?>
+<?php
+class GianHangApp extends AppObject {
+    public $app_name="GianHang";
+    public $dir_layout="tgdd_skin"; 
+    public $layout="chitiet_sanpham";
+    public $page_title = "Trang chủ | Thế giới di động";
+
+    public function __construct() {
+        $this->authen();
+        parent::__construct();
+    }
+
+    public function display(){
+        $this->dir_layout="tgdd_skin";
+        $this->layout = isset($_REQUEST["layout"])?$_REQUEST["layout"]:"chitiet_sanpham"; 
+
+        if(isset($_GET["action"])){
+            $this->{'act'.$_GET["action"]}();
+        }else{
+            $this->view = isset($_REQUEST["view"])?$_REQUEST["view"]:"default"; 
+            parent::display();
+        }  
+    }
+
+}
