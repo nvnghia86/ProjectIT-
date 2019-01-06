95c75bb5bcb012a5726ecfbc279427616d98b64e
phananhson24101995 <phananhson24101995@gmail.com>
2019-01-06 09:45:54 +0700

    Giao dien nhom 1

---
+++ 5_DOAN/Nhom01/3_Code/index.php
@@ -0,0 +1,150 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Trang Chủ</title>
+	<meta charset="utf-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="css/style.css" />
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css" />
+	<script type="text/javascript" src="js/bootstrap.min.js"></script>
+</head>
+<body>
+	<div class="page-header">
+		<div class="page-header_inner">
+			<a href="#"></a>
+			<div class="header-menu">
+				<div class="menu-header-menu-container">
+					<ul class="menu-header-menu">
+						<li><a href="#"></a></li>
+						<li><a href="#"></a></li>
+						<li><a href="#"></a></li>
+						<li><a href="#"></a></li>
+						<li><a href="#"></a></li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div class="page-nav">
+		<div class="inner"></div>
+	</div>
+
+	<div class="slider">
+		<div class="wrapper"></div>
+	</div>
+
+	<div class="content">
+		<div class="page-content">
+			<div class="env-content_top ">
+				<div class="inner"></div>
+			</div>
+
+			<div class="env-content_body">
+				<div class="layout-component">
+					<ul>
+						<li></li>
+						<li></li>
+						<li></li>
+						<li></li>
+						<li></li>
+						<li></li>
+					</ul>
+				</div>
+
+				<div class="page-break">
+					<div class="outer"> </div>
+				</div>
+			</div>
+		</div>
+
+		<div class="page-content">
+			<div class="env-content_top ">
+				<div class="inner"></div>
+			</div>
+
+			<div class="layout-component">
+					<ul>
+						<li></li>
+					</ul>
+			</div>
+
+			<div class="layout-component">
+					<ul>
+						<li></li>
+						<li></li>
+						<li></li>
+					</ul>
+			</div>
+
+			<div class="page-break">
+					<div class="outer"> </div>
+			</div>
+		</div>
+
+		<div class="page-content">
+			<div class="env-content_top ">
+				<div class="inner"></div>
+			</div>
+
+			<div class="layout-left"></div>
+
+			<div class="layout-right"></div>
+
+			<div class="page-break">
+					<div class="outer"> </div>
+			</div>
+		</div>
+
+		<div class="page-content">
+			<div class="env-content_top ">
+				<div class="inner"></div>
+			</div>
+			<div class="layout-component">
+					<ul>
+						<li></li>
+					</ul>
+			</div>
+		</div>
+
+		<div class="env-content_body">
+				<div class="layout-component">
+					<ul>
+						<li></li>
+						<li></li>
+						<li></li>
+						<li></li>
+						<li></li>
+						<li></li>
+					</ul>
+				</div>
+
+				<div class="page-break">
+					<div class="outer"> </div>
+				</div>
+			</div>
+		</div>
+
+
+	</div>
+
+	<div class="footer">
+		<div class="wrapper">
+			<ul class="list-footer">
+				<div class="col"></div>
+				<div class="col"></div>
+				<div class="col"></div>
+				<div class="col"></div>
+				<div class="col"></div>
+			</ul>
+
+			<ul class="list-contact"></ul>
+
+			<div class="cpoyright">
+				<div class="copyright"></div>
+				<div class="copyright-logo"></div>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
